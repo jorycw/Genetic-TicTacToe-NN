45f7b4a7e3e9baa129584729cb0bd0909ff36661
jorycw <jorycw@cs.washington.edu>
2019-03-16 08:36:28 -0700

    main.py finished

--- main.py
+++ main.py
@@ -1,15 +1,42 @@
 from board import Board
 from population import Population
 import numpy as np
+from tqdm import trange
 
 if __name__ == '__main__':
 
+	epoch = 500
+
 	pop = Population()
+	for e in trange(epoch):
 
-	for pair in pop.get_pairs():
+		pop.breed()
+		for pair in pop.get_pairs():
 
-		board = Board()
-		player = 1
+			board = Board()
+			player = 1
 
-		while (not board.is_over()):
-			prob = pair[player](board.)
+			while (not board.is_over()):
+				prob = pair[player](board.state)
+				max_index = np.argmax(prob)[0]
+				
+				while (not board.is_valid(max_index)): # inf loop if all moves are invalid or prob is filled w/ 0s and is_valid(0) == false
+					prob[max_index] = 0
+					max_index = np.argmax(prob)[0]
+
+				board.apply_move(player, max_index)
+				player = player * -1
+
+			res = board.winner()
+
+			if res != 0:
+				pair[(res - 1) // (-2)].winner = True
+				pair[(res + 1) //   2 ].winner = False
+			else:
+				pair[0].winner = None
+				pair[1].winner = None
+
+		pop.cull()
+		
+	pop.gen[0].save_model('winner.model')
+
