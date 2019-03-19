3fb3efced80a20d86bbea2672727414ce30d6b0a
jorycw <jorycw@cs.washington.edu>
2019-03-19 08:46:52 -0700

    winner to score

--- nn.py
+++ nn.py
@@ -19,7 +19,7 @@ class Player(nn.Module):
         self.fc1 = nn.Linear(in_dim, fc1_dim)
         self.fc2 = nn.Linear(fc1_dim, fc2_dim)
         self.fc3 = nn.Linear(fc2_dim, out_dim)
-        self.winner = None # Default None represents tie
+        self.score # Default None represents tie
 
     def forward(self, x):
         """ Completes 1 forward pass through this network.
