dc1938e5ef7c343bbea26a76cf82c41bf0af8933
Jory Williams <45256718+jorycw@users.noreply.github.com>
2019-04-12 02:44:50 -0700

    Update play.py

--- play.py
+++ play.py
@@ -11,18 +11,17 @@ if __name__ == '__main__':
 
     board = Board()
 
-    print('computer goes first cuz hes not very smart')
+    print('computer playing first')
 
     computer_turn = True
 
     while not board.is_over():
         if computer_turn:
-
             prob = computer(board.state)
             max_index = np.argmax(prob)
             max_index = max_index if isinstance(max_index, np.int64) else max_index[0]
             
-            while (not board.is_valid(max_index)): # inf loop if all moves are invalid or prob is filled w/ 0s and is_valid(0) == false
+            while (not board.is_valid(max_index)):
                 prob[max_index] = 0
                 max_index = np.argmax(prob)
                 max_index = max_index if isinstance(max_index, np.int64) else max_index[0]
@@ -31,9 +30,9 @@ if __name__ == '__main__':
             print('computer played in ',  max_index)
 
         else:
-
             move = int(input('your turn, input number between 0-8'))
-            if not board.is_valid(move): print('youre dum')
+            if not board.is_valid(move): 
+                print('invalid input')
             print('you input ', move)
             board.apply_move(-1, move)
 
@@ -43,10 +42,8 @@ if __name__ == '__main__':
     res = board.winner()
 
     if res == 0:
-        print('you couldnt beat a tictactoe bot made with 3 lines of code')
+        print('NN bot lost')
     elif res == 1:
-        print('you lost to a tictactoe bot made with 3 lines of code')
+        print('NN bot won')
     elif res == -1:
-        print('nice! you beat a tictactoe bot made with 3 lines of code!')
-    else:
-        print('somebody screwed up their board.winner() method')
+        print('NN bot lost')
